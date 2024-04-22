ca697e5db2b3510b22e8213821a8bec7390e864a
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-22 22:08:24 +0530

    Remove unused dependencies and update Prisma configuration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,2 @@
-import { Pool } from '@prisma/pg-worker';
-import { PrismaPg } from '@prisma/adapter-pg-worker';
 import { PrismaClient } from '@prisma/client';
-
-const connectionString = `${process.env.DATABASE_URL}`;
-let db;
-
-if (process.env.NODE_ENV === 'production') {
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
-  db = new PrismaClient({ adapter });
-} else {
-  db = new PrismaClient();
-}
-
-export { db };
-
-// const pool = new Pool({ connectionString });
-// const adapter = new PrismaPg(pool);
-// export const db = new PrismaClient({ adapter });
+export const db = new PrismaClient();
