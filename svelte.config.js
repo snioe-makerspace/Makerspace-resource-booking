45c5a9dd23288495163d49d3a7d77c6d93d8a0fe
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-22 20:43:47 +0530

    Remove @sveltejs/adapter-cloudflare and use @sveltejs/adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 
