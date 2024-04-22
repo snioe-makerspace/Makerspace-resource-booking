cc273a0c4c0a294ec54956ce65f4fb3751fec4ba
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-22 13:04:10 +0530

    chore: remove yarn files and add pooling for edge workers

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,2 +1,9 @@
-import prisma from '@prisma/client';
-export const db = new prisma.PrismaClient();
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const db = new PrismaClient({ adapter });
